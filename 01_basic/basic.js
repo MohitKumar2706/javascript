aa416201587b5ef693e99693f167c20c3733e93b
Mohit Kumar <mohitkumar2706@gmail.com>
2025-08-13 17:55:41 +0530

    Add files via upload

---
+++ 01_basic/basic.js
@@ -0,0 +1,41 @@
+
+// !Console.log()
+// console.log("hello World!");
+// console.log("My", "name", "is", "mohit");
+
+
+// let a=20;
+// let b=30;
+// console.log("Your answer is", a+b);
+
+
+
+// ! Template Literals
+/*
+
+let pencil= 20;
+let erasor=10;
+
+let final="The final price is :"+(pencil+erasor)+"rupees";  //without tebplate literals
+console.log(final);
+
+let final2=`the final price is: ${pencil+erasor}rupees`;  //With template literals
+console.log(final2);
+
+*/
+
+// !operators
+
+let age=18;
+console.log(age>=18);
+
+    //comparison operator
+
+let b=5;
+let st='5'
+console.log(b=='5');   //true
+console.log(b==st);    //true
+console.log(b==true);  //false
+
+console.log(b==='5');  //false
+console.log(b===st);  //flase
